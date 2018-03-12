fd0ae242c43f4aa4096844f320b995ffbd621b2e
JeanM <jean.massip@viacesi.fr>
2018-03-12 15:01:06 +0100

    add extensions and pages

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
     include "./includes/header.php";
     $page = $_GET['page'] ?? "";
+    $files = glob("./includes/*.inc.php");
+    var_dump($files);
     include "./includes/$page.php";
     include "./includes/footer.php";
